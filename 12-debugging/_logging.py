a3728d5098509aacc9e3ca8902ba1f32cca6cfad
Matthijs Boet <m.boet@chello.nl>
2020-02-05 22:21:24 +0100

    finished automate the boring udemy course

--- 12-debugging/_logging.py
+++ 12-debugging/_logging.py
@@ -1,12 +1,17 @@
+# cant call file logging.py when importing logging module
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.basicConfig(
+    # filename='error_log.txt',
+    level=logging.DEBUG, 
+    format='%(asctime)s - %(levelname)-s - %(message)s')
 
 logging.debug('Start of program')
 
 def factorial(n):
     logging.debug('Start of factorial(%s)' % (n))
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug('i is %s, total is %s' % (i, total))
     
